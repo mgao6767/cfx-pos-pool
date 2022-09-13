69a406903b04eef6b64c7e01c561898dc5d656fd
Adrian Gao <mingze.gao@sydney.edu.au>
2022-09-13 11:11:32 +1000

    Updates

--- locales/index.js
+++ locales/index.js
@@ -1,37 +1,23 @@
 import i18n from 'i18next'
 import {initReactI18next} from 'react-i18next'
-import en from './en/translation.json';
-import zh from './zh/translation.json';
-import id from './id/translation.json';
-import ko from './ko/translation.json';
-import vn from './vn/translation.json';
-import config from '../../pool.config';
+import en from './en/translation.json'
+import zh from './zh/translation.json'
+import config from '../../pool.config'
 
-i18n
-  .use(initReactI18next)
-  .init({
-    lng: config.defaultLang || 'en',
-    debug: true,
-    resources: {
-      en: {
-        translation: en
-      },
-      zh: {
-        translation: zh
-      },
-      id: {
-        translation: id
-      },
-      vn: {
-        translation: vn
-      },
-      ko: {
-        translation: ko
-      },
+i18n.use(initReactI18next).init({
+  lng: config.defaultLang || 'en',
+  debug: true,
+  resources: {
+    en: {
+      translation: en,
     },
-    interpolation: {
-      escapeValue: false,
+    zh: {
+      translation: zh,
     },
-  })
+  },
+  interpolation: {
+    escapeValue: false,
+  },
+})
 
 export default i18n
